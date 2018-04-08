3015e451a599c281c7194f9929bb2519ef1d4917
Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
2018-04-08 18:32:44 +0200

    create new controller class which extend symfony controller class

---
+++ src/Controller/Controller.php
@@ -0,0 +1,68 @@
+<?php
+  /**
+   * Created by PhpStorm.
+   * @author: Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
+   * @date  : 08/04/18
+   * @time  : 18:23
+   */
+
+  namespace App\Controller;
+
+
+  use Symfony\Bundle\FrameworkBundle\Routing\Router;
+
+  class Controller extends \Symfony\Bundle\FrameworkBundle\Controller\Controller
+  {
+    #region const
+    #endregion
+
+
+    #region public properties
+    #endregion
+
+
+    #region protected properties
+    #endregion
+
+
+    #region private properties
+    #endregion
+
+
+    #region magic methods
+    #endregion
+
+
+    #region getters/setters
+    #endregion
+
+
+    #region public methods
+
+    /**
+     * @param null|string $name
+     * @return \Doctrine\Common\Persistence\ObjectManager
+     */
+    protected function getManager($name)
+    {
+      return $this->getDoctrine()->getManager($name);
+    }
+
+    /**
+     * @return Router|object
+     */
+    protected function getRouter()
+    {
+      return $this->get('router');
+    }
+
+    #endregion
+
+
+    #region protected methods
+    #endregion
+
+
+    #region private methods
+    #endregion
+  }
