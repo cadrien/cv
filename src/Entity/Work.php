45e43b9778dc8fc404adfccf39fa9ef5f0626cf9
Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
2018-04-20 19:30:50 +0200

    create Work and WorkLine entity

---
+++ src/Entity/Work.php
@@ -0,0 +1,239 @@
+<?php
+  /**
+   * Created by PhpStorm.
+   * @author: Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
+   * @date  : 09/04/18
+   * @time  : 08:22
+   */
+
+  namespace App\Entity;
+
+  use Doctrine\ORM\Mapping as ORM;
+  use App\Extension\Doctrine\ArrayCollection;
+
+  /**
+   * Class Work
+   * @package AppBundle\Entity
+   *
+   * @ORM\Table(name="work")
+   * @ORM\Entity
+   */
+  class Work extends Entity
+  {
+    #region const
+    #endregion
+
+
+    #region public properties
+    #endregion
+
+
+    #region protected properties
+    #endregion
+
+
+    #region private properties
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="wor_id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="wor_from", type="date")
+     */
+    private $from;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="wor_to", type="date", nullable=true)
+     */
+    private $to;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="wor_job_name", type="string")
+     */
+    private $jobName;
+
+    /**
+     * @var Company
+     *
+     * @ORM\ManyToOne(targetEntity="Company")
+     * @ORM\JoinColumn(name="com_id", referencedColumnName="com_id", nullable=false)
+     */
+    private $company;
+
+    /**
+     * @var WorkLine[]|ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="WorkLine", mappedBy="work")
+     */
+    private $workLines;
+
+    /**
+     * @var null|\DateInterval
+     */
+    private $duration = null;
+
+    #endregion
+
+
+    #region magic methods
+    #endregion
+
+
+    #region getters/setters
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+      return $this->id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getFrom()
+    : \DateTime
+    {
+      return $this->from;
+    }
+
+    /**
+     * @param \DateTime $from
+     */
+    public function setFrom(\DateTime $from)
+    {
+      $this->from = $from;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getTo()
+    {
+      return $this->to;
+    }
+
+    /**
+     * @param \DateTime|null $to
+     * @return $this
+     */
+    public function setTo(\DateTime $to = null)
+    {
+      $this->to = $to;
+      return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJobName()
+    : string
+    {
+      return $this->jobName;
+    }
+
+    /**
+     * @param string $jobName
+     */
+    public function setJobName(string $jobName)
+    {
+      $this->jobName = $jobName;
+    }
+
+    /**
+     * @return Company
+     */
+    public function getCompany()
+    : Company
+    {
+      return $this->company;
+    }
+
+    /**
+     * @param Company $company
+     */
+    public function setCompany(Company $company)
+    {
+      $this->company = $company;
+    }
+
+    /**
+     * @return WorkLine[]|ArrayCollection
+     */
+    public function getWorkLines()
+    : ArrayCollection
+    {
+      return new ArrayCollection($this->workLines->toArray());
+    }
+
+    /**
+     * @param WorkLine $workLine
+     * @return $this
+     */
+    public function addWorkLine(WorkLine $workLine)
+    {
+      $workLine->setWork($this);
+      $this->workLines->add($workLine);
+      return $this;
+    }
+
+    /**
+     * @param WorkLine $workLine
+     * @return $this
+     */
+    public function removeWorkLine(WorkLine $workLine)
+    {
+      $this->workLines->removeElement($workLine);
+      return $this;
+    }
+
+    /**
+     * @return WorkLine[]|ArrayCollection
+     */
+    public function getWorkLinesFirstLevel()
+    : ArrayCollection
+    {
+      return $this->getWorkLines()->filter(function(WorkLine $workLine) {
+        return $workLine->getParent() === null;
+      });
+    }
+
+    /**
+     * @return bool|\DateInterval
+     */
+    public function getDuration()
+    {
+      if($this->duration === null)
+        $this->duration = ( $this->to === null ? new \DateTime() : $this->to )->diff($this->from);
+      return $this->duration;
+    }
+
+    #endregion
+
+
+    #region public methods
+    #endregion
+
+
+    #region protected methods
+    #endregion
+
+
+    #region private methods
+    #endregion
+  }
---
+++ src/Entity/WorkLine.php
@@ -0,0 +1,188 @@
+<?php
+  /**
+   * Created by PhpStorm.
+   * @author: Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
+   * @date  : 09/04/18
+   * @time  : 08:27
+   */
+
+  namespace App\Entity;
+
+  use App\Extension\Doctrine\ArrayCollection;
+  use Doctrine\ORM\PersistentCollection;
+
+
+  /**
+   * Class Work
+   * @package AppBundle\Entity
+   *
+   * @ORM\Table(name="work_line")
+   * @ORM\Entity(repositoryClass="AppBundle\Repository\WorkLineRepository")
+   */
+  class WorkLine extends Entity
+  {
+    #region const
+    #endregion
+
+
+    #region public properties
+    #endregion
+
+
+    #region protected properties
+    #endregion
+
+
+    #region private properties
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="wol_id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var Work
+     *
+     * @ORM\ManyToOne(targetEntity="Work")
+     * @ORM\JoinColumn(name="wor_id", referencedColumnName="wor_id", nullable=false)
+     */
+    private $work;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="wor_label", type="string")
+     */
+    private $label;
+
+    /**
+     * @var WorkLine|null
+     *
+     * @ORM\ManyToOne(targetEntity="WorkLine")
+     * @ORM\JoinColumn(name="wol_id_parent", referencedColumnName="wol_id", nullable=true)
+     */
+    private $parent;
+
+    /**
+     * @var WorkLine[]|PersistentCollection|ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="WorkLine", mappedBy="parent")
+     */
+    private $workLines;
+
+    #endregion
+
+
+    #region magic methods
+    #endregion
+
+
+    #region getters/setters
+
+    /**
+     * @return int
+     */
+    public function getId()
+    : int
+    {
+      return $this->id;
+    }
+
+    /**
+     * @return Work
+     */
+    public function getWork()
+    : Work
+    {
+      return $this->work;
+    }
+
+    /**
+     * @param Work $work
+     */
+    public function setWork(Work $work)
+    {
+      $this->work = $work;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    : string
+    {
+      return $this->label;
+    }
+
+    /**
+     * @param string $label
+     */
+    public function setLabel(string $label)
+    {
+      $this->label = $label;
+    }
+
+    /**
+     * @return WorkLine|null
+     */
+    public function getParent()
+    {
+      return $this->parent;
+    }
+
+    /**
+     * @param WorkLine|null $parent
+     */
+    public function setParent(WorkLine $parent = null)
+    {
+      $this->parent = $parent;
+    }
+
+    /**
+     * @return WorkLine[]
+     */
+    public function getWorkLines()
+    : array
+    {
+      return $this->workLines->toArray();
+    }
+
+    /**
+     * @param WorkLine $workLine
+     * @return $this
+     */
+    public function addWorkLine(WorkLine $workLine)
+    {
+      $workLine->setWork($this->getWork());
+      $this->workLines->add($workLine);
+      return $this;
+    }
+
+    /**
+     * @param WorkLine $workLine
+     * @return $this
+     */
+    public function removeWorkLine(WorkLine $workLine)
+    {
+      $this->workLines->removeElement($workLine);
+      return $this;
+    }
+
+    #endregion
+
+
+    #region public methods
+    #endregion
+
+
+    #region protected methods
+    #endregion
+
+
+    #region private methods
+    #endregion
+  }
