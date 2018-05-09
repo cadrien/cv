3394d4cd036190174cc888a7e7063a757d76ed01
Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
2018-05-09 17:30:11 +0200

    create competence page

---
+++ src/Controller/CompetenceController.php
@@ -0,0 +1,67 @@
+<?php
+  /**
+   * Created by PhpStorm.
+   * @author: Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
+   * @date  : 26/04/18
+   * @time  : 08:09
+   */
+
+  namespace App\Controller;
+
+  use App\Entity\CategoryCompetence;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+  use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\VarDumper\VarDumper;
+
+  class CompetenceController extends Controller
+  {
+    #region const
+    #endregion
+
+
+    #region public properties
+    #endregion
+
+
+    #region protected properties
+    #endregion
+
+
+    #region private properties
+    #endregion
+
+
+    #region magic methods
+    #endregion
+
+
+    #region getters/setters
+    #endregion
+
+
+    #region public methods
+
+    /**
+     * @Route("/competence", name="competence")
+     * @Method("GET")
+     * @return mixed[]
+     */
+    public function indexAction()
+    {
+      return new Response($this->renderView('competence.html.twig', ['categories' => $this->getManager()
+                                                                                          ->getRepository(CategoryCompetence::class)
+                                                                                          ->findAll()]));
+    }
+
+    #endregion
+
+
+    #region protected methods
+    #endregion
+
+
+    #region private methods
+    #endregion
+  }
