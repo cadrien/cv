cc36a6629edca3c9757d25d6909baddbb525bfa5
Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
2018-05-09 17:31:41 +0200

    create work page

---
+++ src/Controller/WorkController.php
@@ -0,0 +1,112 @@
+<?php
+  /**
+   * Created by PhpStorm.
+   * @author: Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
+   * @date  : 08/04/18
+   * @time  : 18:35
+   */
+
+  namespace App\Controller;
+
+
+  use App\Entity\Company;
+  use App\Entity\Work;
+  use App\Extension\Doctrine\ArrayCollection;
+  use Symfony\Component\HttpFoundation\JsonResponse;
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\VarDumper\VarDumper;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+  class WorkController extends Controller
+  {
+    #region const
+    #endregion
+
+
+    #region public properties
+    #endregion
+
+
+    #region protected properties
+    #endregion
+
+
+    #region private properties
+
+    private $works = null;
+
+    #endregion
+
+
+    #region magic methods
+    #endregion
+
+
+    #region getters/setters
+    #endregion
+
+
+    #region public methods
+
+    /**
+     * @param Request $request
+     * @Route("/", name="homepage")
+     * @return JsonResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function workAction(Request $request)
+    {
+      if($request->isXmlHttpRequest()) {
+        return new JsonResponse([
+                                  'html' => $this->renderView('work.html.twig',
+                                                              ['companies' => $this->getCompaniesSorted()])
+                                ]);
+      }
+      return $this->render('work.html.twig', ['companies' => $this->getCompaniesSorted()]);
+    }
+
+    #endregion
+
+
+    #region protected methods
+
+    /**
+     * @return Work[]|ArrayCollection
+     */
+    protected function getWorks()
+    : ArrayCollection
+    {
+      $works = $this->getManager()->getRepository(Work::class)->findAll();
+      return $this->works = ( new ArrayCollection($works) )->usort(function(Work $w1, Work $w2) {
+        return $w1->getFrom() < $w2->getFrom();
+      });
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    protected function getCompanies()
+    : ArrayCollection
+    {
+      return $this->getWorks()->map(function(Work $work) {
+        return $work->getCompany();
+      })->unique();
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    protected function getCompaniesSorted()
+    : ArrayCollection
+    {
+      return $this->getCompanies()->usort(function(Company $c1, Company $c2) {
+        return $c1->getWorksSorted()->last()->getFrom() < $c2->getWorksSorted()->last()->getFrom();
+      });
+    }
+
+    #endregion
+
+
+    #region private methods
+    #endregion
+  }
