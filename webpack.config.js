ca2bbaa5d8c5e78a5bd26b52ef2cd2a1fcd730bd
Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
2018-05-10 17:21:15 +0200

    add vertical timeline js in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,9 @@ Encore
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
-    .addEntry('vertical-timeline_reset', './assets/vertical-timeline-master/css/reset.css')
-    .addEntry('vertical-timeline_style', './assets/vertical-timeline-master/css/style.css')
+    .addEntry('vertical-timeline-reset', './assets/vertical-timeline-master/css/reset.css')
+    .addEntry('vertical-timeline-style', './assets/vertical-timeline-master/css/style.css')
+    .addEntry('vertical-timeline-js', './assets/vertical-timeline-master/js/main.js')
     .addEntry('fa', './vendor/fortawesome/font-awesome/css/font-awesome.css')
     .addEntry('bootstrap', './vendor/twbs/bootstrap/dist/css/bootstrap.css')
     .addEntry('jquery.min', './vendor/components/jquery/jquery.min.js')
