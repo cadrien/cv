f1c26c85422d4f799bb37fc99dcb708e167447e1
Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
2018-05-13 19:54:29 +0200

    add postUp method in doctrine migration

--- src/Migrations/Version20180513174421.php
+++ src/Migrations/Version20180513174421.php
@@ -25,4 +25,33 @@ final class Version20180513174421 extends AbstractMigration
 
         $this->addSql('ALTER TABLE formation DROP frm_url');
     }
+
+    /**
+     * @param Schema $schema
+     */
+    public function postUp(Schema $schema)
+    {
+      parent::postUp($schema);
+
+      try {
+        $this->connection->executeQuery('
+            UPDATE formation
+            SET frm_url = "http://www.unige.ch/sciences/fr/enseignements/formations/masters/info/"
+            WHERE frm_id = 1
+        ');
+        $this->connection->executeQuery('
+            UPDATE formation
+            SET frm_url = "https://fac-sciences.univ-st-etienne.fr/fr/formations/licence-XA/sciences-technologies-sante-STS/licence-informatique-program-licence-informatique.html"
+            WHERE frm_id = 2
+        ');
+        $this->connection->executeQuery('
+            UPDATE formation
+            SET frm_url = "http://www.mauriac.org/"
+            WHERE frm_id = 3
+        ');
+      }
+      catch(\Doctrine\DBAL\DBALException $e) {
+        $this->write($e->getMessage());
+      }
+    }
 }
