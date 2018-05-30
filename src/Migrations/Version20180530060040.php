32ef8d695bf9a772852b8f9b9b2874ecf31166d4
Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
2018-05-30 08:09:40 +0200

    add new doctrine migration which insert github line in info table

---
+++ src/Migrations/Version20180530060040.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180530060040 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+      $this->addSql('INSERT INTO info ("inf_typ", "inf_value") VALUE ("social_network", "a:3:{s:4:"logo";s:6:"github";s:3:"url";s:27:"https://github.com/cadrien/";s:4:"name";s:6:"GitHub";}")');
+    }
+
+    public function down(Schema $schema) : void
+    {}
+}
