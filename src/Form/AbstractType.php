289b37db251c0956147f8a52dce2d8f2030fb257
Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
2018-06-05 18:02:54 +0200

    override AbstractType class

---
+++ src/Form/AbstractType.php
@@ -0,0 +1,48 @@
+<?php
+  /**
+   * Created by PhpStorm.
+   * @author: Adrien CHAUMARAT <adrien.chaumarat@gmail.com>
+   * @date  : 02/06/18
+   * @time  : 19:54
+   */
+
+  namespace App\Form;
+
+
+  abstract class AbstractType extends \Symfony\Component\Form\AbstractType
+  {
+    #region const
+    #endregion
+
+
+    #region public properties
+    #endregion
+
+
+    #region protected properties
+    #endregion
+
+
+    #region private properties
+    #endregion
+
+
+    #region magic methods
+    #endregion
+
+
+    #region getters/setters
+    #endregion
+
+
+    #region public methods
+    #endregion
+
+
+    #region protected methods
+    #endregion
+
+
+    #region private methods
+    #endregion
+  }
